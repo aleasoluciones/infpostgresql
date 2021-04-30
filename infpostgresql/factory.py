886882156a63276918b2050dd6ec2888b75ff88f
luisδμ <luisdedios@alea-soluciones.com>
2021-04-30 09:34:53 +0200

    [REFACTOR] Use fstrings

--- infpostgresql/factory.py
+++ infpostgresql/factory.py
@@ -1,22 +1,20 @@
-import os
 import psycopg2.extras
-
-from infpostgresql.client import PostgresClient
 
 from infcommon.factory import Factory
 
+from infpostgresql.client import PostgresClient
+
 
 def postgres_client_from_connection_parameters(user=None, password=None, host=None, port=None, db_name=None, use_dict_cursor=None):
-    connection_uri = 'postgresql://{user}:{password}@{host}:{port}/{db_name}'.format(user=user, password=password, host=host, port=port, db_name=db_name)
+    connection_uri = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
     return _postgres_client(connection_uri, use_dict_cursor)
 
 
 def _postgres_client(connection_uri=None, use_dict_cursor=None):
     cursor_factory = _cursor_factory(use_dict_cursor)
-    instance_id = 'postgres_client_{connection_uri}_{use_dict_cursor}'.format(connection_uri=connection_uri, use_dict_cursor=use_dict_cursor)
+    instance_id = f'postgres_client_{connection_uri}_{use_dict_cursor}'
     return Factory.instance(instance_id,
-                            lambda: PostgresClient(connection_uri, cursor_factory=cursor_factory)
-                           )
+                            lambda: PostgresClient(connection_uri, cursor_factory=cursor_factory))
 
 
 def _cursor_factory(use_dict_cursor=None):
