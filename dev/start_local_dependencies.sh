c9e23f8bc69d49d9cf63fddd2727872fb68badb0
luisddm <luisdedios@alea-soluciones.com>
2021-10-18 08:37:08 +0200

    [INTERNAL] Cosmetic improvements

--- dev/start_local_dependencies.sh
+++ dev/start_local_dependencies.sh
@@ -14,10 +14,10 @@ TIMEOUT=30
 printf "Checking port ${POSTGRES_PORT} ... "
 if [[ $(uname) == 'Linux' ]]; then
     timeout ${TIMEOUT} bash -c "until echo > /dev/tcp/localhost/${POSTGRES_PORT}; do sleep 0.5; done" > /dev/null 2>&1
-    [[ $? -eq 0 ]] && echo -e '\e[1;32mOK\e[0m' || echo -e '\e[1;31mNOK\e[0m'
+    [[ $? -eq 0 ]] && echo -e '\e[32mOK\e[0m' || echo -e '\e[31mNOK\e[0m'
 elif [[ -x $(command -v nc) ]]; then
     timeout ${TIMEOUT} bash -c "until nc -vz ${POSTGRES_HOSTNAME} ${POSTGRES_PORT}; do sleep 0.5; done" > /dev/null 2>&1
-    [[ $? -eq 0 ]] && echo -e '\e[1;32mOK\e[0m' || echo -e '\e[1;31mNOK\e[0m'
+    [[ $? -eq 0 ]] && echo -e '\e[32mOK\e[0m' || echo -e '\e[31mNOK\e[0m'
 else
     echo -e "Unable to check port ${POSTGRES_PORT}. Just sleeping for 5 seconds ..."
     sleep 5
