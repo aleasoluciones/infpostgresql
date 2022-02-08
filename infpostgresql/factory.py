d45ec41d4055d537a1bee762e084007a4292959c
luisddm <2978951+luisddm@users.noreply.github.com>
2022-02-08 10:48:00 +0100

    [FEATURE] Add another factory which makes use of the connection uri

--- infpostgresql/factory.py
+++ infpostgresql/factory.py
@@ -19,6 +19,15 @@ def postgres_client_from_connection_parameters(user=None,
     )
 
 
+def postgres_client_from_connection_uri(connection_uri=None,
+                                        use_dict_cursor=False):
+    cursor_factory = _cursor_factory(use_dict_cursor)
+    return Factory.instance(
+        f'postgres_client_{connection_uri}_{use_dict_cursor}',
+        lambda: PostgresClient(connection_uri, cursor_factory=cursor_factory)
+    )
+
+
 def _cursor_factory(use_dict_cursor=False):
     if use_dict_cursor:
         return psycopg.rows.dict_row
